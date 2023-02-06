60ce6f367457b7bc784c3106a8c09cd97d766311
Henry <124216714+imHenryMa@users.noreply.github.com>
2023-02-06 00:23:46 -0500

    Complete extra instructions for rock, paper, scissors project for The Odin Project - Javascript Foundations

--- 3-javascript-basics/project-rock-paper-scissors/script.js
+++ 3-javascript-basics/project-rock-paper-scissors/script.js
@@ -34,16 +34,18 @@ function getPlayerChoice(){
 }
 
 function playGame(){
-    alert('Let\'s play rock, paper scissors!');
+    //alert('Let\'s play rock, paper scissors!');
     let playerChoice = getPlayerChoice();
     alert(`You selected ${playerChoice}`);
     alert('Now the computer is going to select a choice..');
     let computerChoice = getComputerChoice();
     alert(`The computer selected ${computerChoice}`);
 
+    let result;
 
     if(playerChoice == computerChoice){
         playerWin = 'Tied!';
+        result = 0;
     } else {
         switch (computerChoice){
             case 'rock':
@@ -58,8 +60,46 @@ function playGame(){
             default:
                 break;
         }
+        result = playerWin? 1 : -1;
         playerWin = playerWin ? 'Won!' : 'Lost!';
     }
 
     alert(`You ${playerWin}`);
+    return result;
+}
+
+function playGameBestOf(rounds){
+    alert(`Let\'s play rock, paper scissors! This will be a game to see the best of ${rounds}`);
+    let playerScore = 0;
+    let computerScore =0;
+
+    let winner;
+    for(let current = 1; current <= rounds; current++){
+        alert(`Round ${current}`);
+
+        winner = playGame();
+
+        switch(winner){
+            case -1:
+                computerScore++
+                break;
+            case 1:
+                playerScore++;
+                break;
+            default:
+                break;
+        }
+
+        if(playerScore > Math.floor(rounds/2) || computerScore > Math.floor(rounds/2)) break;
+    }
+
+    if(playerScore > computerScore){
+        winner = "the Player";
+    } else if (computerScore > playerScore){
+        winner = "the Computer";
+    } else {
+        winner = "no one (it was a draw)";
+    }
+
+    alert(`The overwall winner was.. ${winner}!`)
 }
