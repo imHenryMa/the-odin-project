6039ec0691fa1cd742a8f943056b2847f98f56aa
Henry <124216714+imHenryMa@users.noreply.github.com>
2023-02-06 23:55:30 -0500

    Complete assignment for 'Revisiting Rock Paper Scissors' lesson of The Odin Project - CSS Foundations

--- 3-javascript-basics/project-rock-paper-scissors/script.js
+++ 3-javascript-basics/project-rock-paper-scissors/script.js
@@ -1,3 +1,28 @@
+let maxScore = 4;
+let computerScore = 0;
+let playerScore = 0;
+
+///Find all the buttons
+const buttons = []
+buttons.push(document.querySelector('#rock'));
+buttons.push(document.querySelector('#paper'));
+buttons.push(document.querySelector('#scissors'));
+
+//Give then listeners
+for(const button of buttons){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        playerChoice = e.target.id;
+        console.log(playerChoice);
+        playGame(playerChoice);
+    })
+}
+
+//Finds the score text for the computer and player
+computerText = document.querySelector('#cpu-score');
+console.log(computerText);
+playerText = document.querySelector('#player-score');
+
 function randomNumber(){
     let random = Math.floor(Math.random()*3)+1;
     console.log(random);
@@ -33,13 +58,9 @@ function getPlayerChoice(){
     return playerChoice;
 }
 
-function playGame(){
-    //alert('Let\'s play rock, paper scissors!');
-    let playerChoice = getPlayerChoice();
-    alert(`You selected ${playerChoice}`);
-    alert('Now the computer is going to select a choice..');
+function playGame(playerChoice){
+
     let computerChoice = getComputerChoice();
-    alert(`The computer selected ${computerChoice}`);
 
     let result;
 
@@ -63,11 +84,45 @@ function playGame(){
         result = playerWin? 1 : -1;
         playerWin = playerWin ? 'Won!' : 'Lost!';
     }
+    
+    switch(result){
+        case -1:
+            computerText.innerText = ++computerScore;
+            break;
+        case 1:
+            playerText.innerText= ++playerScore;
+            break;
+        default:
+            break;
+    }
 
-    alert(`You ${playerWin}`);
-    return result;
+    if(playerScore > maxScore || computerScore > maxScore){
+        
+        if(playerScore > computerScore){
+            winner = "the Player";
+        } if (computerScore > playerScore){
+            winner = "the Computer";
+        }
+
+        for(const button of buttons){
+            button.style.visibility='hidden';
+        }
+
+        alert(`The overwall winner was.. ${winner}!`)
+        const response = confirm("Would you like to play again?");
+        if(response){
+            computerScore = 0;
+            playerScore = 0;
+            computerText.innerText = computerScore;
+            playerText.innerText= playerScore;
+            for(const button of buttons){
+                button.style.visibility='visible';
+            }
+        } 
+    }
 }
 
+/*
 function playGameBestOf(rounds){
     alert(`Let\'s play rock, paper scissors! This will be a game to see the best of ${rounds}`);
     let playerScore = 0;
@@ -103,3 +158,4 @@ function playGameBestOf(rounds){
 
     alert(`The overwall winner was.. ${winner}!`)
 }
+*/
